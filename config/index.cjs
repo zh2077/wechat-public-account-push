906ea69fad22d6c833aa767f40d9338175bc6775
zh2077 <76514624+zh2077@users.noreply.github.com>
2023-11-15 10:59:38 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,8 +13,8 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '93b6d9b242fe2ed8a44c9e06c69f1208',
 
-  PROVINCE: '山东省',
-  CITY: '青岛市',
+  PROVINCE: '辽宁省',
+  CITY: '锦州市',
 
   USERS: [
     {
@@ -23,9 +23,9 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oLsmv6Qj8g6NCyMezr8mBGD2tyP8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '7JPlK-bUqpwaGDluHIyt_11OsIgaidnoSvK2GeGwfk8',
+      useTemplateId: 'DkPX4ZctacytQd_swz3_DB4dPdaBuSD6BdYV0lSyask',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '11-15',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
@@ -36,13 +36,13 @@ const USER_CONFIG = {
           type: '生日', name: '李四', year: '1996', date: '09-31',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2023', date: '11-15',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2023-11-15' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
