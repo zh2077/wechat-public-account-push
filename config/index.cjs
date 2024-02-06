59ea8203609394409724b8bc36e54a1269c519d3
zh2077 <76514624+zh2077@users.noreply.github.com>
2024-02-06 13:25:26 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,7 +19,7 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '胖胖',
+      name: '小宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oLsmv6fGmY300b_AasXWRnfSCMmk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
